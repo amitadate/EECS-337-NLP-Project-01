c70f094989cd3641aa0df7cb48a419ed4ab5f71d
mayankmalik01 <43692802+mayankmalik01@users.noreply.github.com>
2019-02-17 13:38:53 -0600

    Update winner.py
    updated_file

--- src/winner.py
+++ src/winner.py
@@ -1,6 +1,10 @@
 def get_winner_new(year):
 
-    award_list = [ 'best motion picture - drama','cecil b. demille award','best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama','best performance by an actress in a motion picture - comedy or musical', 'best performance by an actor in a motion picture - comedy or musical', 'best motion picture - comedy or musical','best animated feature film', 'best foreign language film', 'best performance by an actress in a supporting role in a motion picture', 'best performance by an actor in a supporting role in a motion picture', 'best director - motion picture', 'best original score - motion picture', 'best screenplay - motion picture','best original song - motion picture', 'best television series - drama', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best television series - comedy or musical', 'best performance by an actress in a television series - comedy or musical', 'best performance by an actor in a television series - comedy or musical', 'best mini-series or motion picture made for television', 'best performance by an actress in a mini-series or motion picture made for television', 'best performance by an actor in a mini-series or motion picture made for television', 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']
+    award_list = OFFICIAL_AWARDS
+    
+    if not get_tweets(year):
+        print("No data present")
+        return None
     
     winners = {}
     for award in award_list:
